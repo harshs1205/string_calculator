efefacfd1303c6dcc8a4c4ba1f31fc576c9ba43c
Harsh <harsh@Joshs-MacBook-Pro-8.local>
2025-09-14 00:26:53 +0530

    add spec for custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,9 @@ RSpec.describe StringCalculator do
     it 'allows newlines as delimiters' do 
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom single character delimiters' do
+      expect(StringCalculator.add("//;\n1l;2")).to eq(3)
+    end
   end
 end
