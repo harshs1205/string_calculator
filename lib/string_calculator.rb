0893bd1309cb61941623aeb6930e7bcf49bbeffe
Harsh <harsh@Joshs-MacBook-Pro-8.local>
2025-09-14 01:31:00 +0530

    refactor: extract helpers for readability and validation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,23 +1,34 @@
 class StringCalculator
-  def self.add(numbers)
-    return 0 if numbers.empty?
+  def self.add(input)
+    return 0 if input.nil? || input.empty?
 
-    delimiter_patterns = /,|\n/
-    if numbers.start_with?("//")
-      header, rest = numbers.split("\n", 2)
-      custom = header[2..-1]
-      delimiter_patterns = Regexp.new(Regexp.escape(custom))
-      numbers = rest 
-    end
+    delimiter = parse_delimiter(input)
+    numbers = tokenize(input, delimiter)
 
-    parts = numbers.split(delimiter_patterns)
-    numbers = parts.map(&:to_i)
-
-    negatives = numbers.select { |n| n < 0 }
-    if negatives.any?
-      raise "Negative numbers are not allowed #{negatives.join(', ')}"
-    end
-
+    check_negatives!(numbers)
     numbers.sum
+  end
+
+  def self.parse_delimiter(input)
+    if input.start_with?("//")
+      header, = input.split("\n", 2)
+      Regexp.new(Regexp.escape(header[2..-1]))
+    else
+      /,|\n/
+    end
+  end
+
+  def self.tokenize(input, delimiter)
+    if input.start_with?("//")
+      _, rest = input.split("\n", 2)
+      rest.split(delimiter).map(&:to_i)
+    else
+      input.split(delimiter).map(&:to_i)
+    end
+  end
+
+    def self.check_negatives!(numbers)
+    negatives = numbers.select { |n| n < 0 }
+    raise "Negative numbers are not allowed #{negatives.join(', ')}" if negatives.any?
   end
 end
